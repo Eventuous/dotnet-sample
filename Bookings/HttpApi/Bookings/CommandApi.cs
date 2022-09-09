99603024a52f73587b2e3a596135c4120250ba10
Alexey Zimarev <alex@zimarev.com>
2022-09-09 10:19:35 +0200

    Update to latest Eventuous Update OTel packages and new Prometheus exporter package

--- Bookings/HttpApi/Bookings/CommandApi.cs
+++ Bookings/HttpApi/Bookings/CommandApi.cs
@@ -16,6 +16,14 @@ public class CommandApi : CommandHttpApiBase<Booking> {
     public Task<ActionResult<Result>> BookRoom([FromBody] BookRoom cmd, CancellationToken cancellationToken)
         => Handle(cmd, cancellationToken);
 
+    /// <summary>
+    /// This endpoint is for demo purposes only. The normal flow to register booking payments is to submit
+    /// a command via the Booking.Payments HTTP API. It then gets propagated to the Booking aggregate
+    /// via the integration messaging flow.
+    /// </summary>
+    /// <param name="cmd">Command to register the payment</param>
+    /// <param name="cancellationToken">Cancellation token</param>
+    /// <returns></returns>
     [HttpPost]
     [Route("recordPayment")]
     public Task<ActionResult<Result>> RecordPayment(
